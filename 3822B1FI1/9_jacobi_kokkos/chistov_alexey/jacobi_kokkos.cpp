09abcde6dfe8abdcce4bb1363614534537760d34
Alexey Chistov <alexey.chistov10@gmail.com>
2026-03-04 22:02:17 +0300

    lab 7,8,9

---
+++ 3822B1FI1/9_jacobi_kokkos/chistov_alexey/jacobi_kokkos.cpp
@@ -0,0 +1,77 @@
+#include "jacobi_kokkos.h"
+#include <cmath>
+
+std::vector<float> JacobiKokkos(const std::vector<float>& a,const std::vector<float>& b,float accuracy) {
+    using ExecSpace = Kokkos::SYCL;
+    using MemSpace  = Kokkos::SYCLDeviceUSMSpace;
+
+    const int n = b.size();
+
+    Kokkos::View<float**, Kokkos::LayoutLeft, MemSpace> d_a("A", n, n);
+    Kokkos::View<float*, MemSpace> d_b("b", n);
+    Kokkos::View<float*, MemSpace> x_old("x_old", n);
+    Kokkos::View<float*, MemSpace> x_new("x_new", n);
+    Kokkos::View<float*, MemSpace> inv_diag("inv_diag", n);
+
+    auto h_a = Kokkos::create_mirror_view(d_a);
+    auto h_b = Kokkos::create_mirror_view(d_b);
+
+    for (int i = 0; i < n; ++i) {
+        h_b(i) = b[i];
+        for (int j = 0; j < n; ++j) {
+            h_a(i, j) = a[i * n + j];
+        }
+    }
+
+    Kokkos::deep_copy(d_a, h_a);
+    Kokkos::deep_copy(d_b, h_b);
+
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecSpace>(0, n),
+        KOKKOS_LAMBDA(int i) {
+            inv_diag(i) = 1.0f / d_a(i, i);
+            x_old(i) = 0.0f;
+        });
+
+    float error = 0.0f;
+
+    for (int iter = 0; iter < ITERATIONS; ++iter) {
+
+        Kokkos::parallel_reduce(
+            Kokkos::RangePolicy<ExecSpace>(0, n),
+            KOKKOS_LAMBDA(int i, float& local_max) {
+
+                float sigma = 0.0f;
+
+                for (int j = 0; j < n; ++j) {
+                    if (j != i)
+                        sigma += d_a(i, j) * x_old(j);
+                }
+
+                float new_val = (d_b(i) - sigma) * inv_diag(i);
+                x_new(i) = new_val;
+
+                float diff = fabsf(new_val - x_old(i));
+                if (diff > local_max)
+                    local_max = diff;
+            },
+            Kokkos::Max<float>(error)
+        );
+
+        Kokkos::fence();
+
+        if (error < accuracy)
+            break;
+
+        std::swap(x_old, x_new);
+    }
+
+    auto h_x = Kokkos::create_mirror_view_and_copy(
+        Kokkos::HostSpace(), x_old);
+
+    std::vector<float> result(n);
+    for (int i = 0; i < n; ++i)
+        result[i] = h_x(i);
+
+    return result;
+}
